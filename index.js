a0e66e4659116f1a9b7f9bf6b58898cc8c7fe5bd
aagaman1 <130974749+aagaman1@users.noreply.github.com>
2023-04-30 17:14:40 +0545

    this is for arays, objects and intro to functions

---
+++ index.js
@@ -0,0 +1,67 @@
+// // console.log("hello")
+// // console.log("hello")
+
+// // course = "hey planet"
+// // age = 123
+// // // active = true
+
+// // console.log("course is", course)
+// // console.log("age is", age)
+// // console.log("this is", active)
+
+// user = 'aagaman'
+// id = 20
+
+// isactive = true
+
+
+
+// console.log('the user', user, 'with the id of', id, 'is', isactive)
+
+// let numbers = ["apple", "banana", "leaf", "lg"]
+// // index//
+
+// console.log("first element:", numbers[2], { name: "apple", price: 20000, object: "phone" })
+// numbers[3] = "sony"
+// console.log(numbers)
+
+// let course = {
+//   name: "mern",
+//   duration: 2.5
+// }
+
+// course.teacher = "someone"
+// course.subject="js"
+// course.name = "3"
+// console.log("course name:", course.name, "course duration:", course.duration, course.teacher)
+
+// let number1 = 1
+// let number2 = 2
+// let numbersum = number1 + number2
+// console.log(number1, "+", number2, "=", number1 + number2, numbersum, "* 2 =", numbersum * 2)
+// number1 = number1 + 1
+// number2 = number2 + 2
+// numbersum = number1 + number2
+// console.log(number1, "+", number2, "=", number1 + number2, numbersum, "* 2 =", numbersum * 2)
+// number1 = number1 + 1
+// number2 = number2 + 2
+// numbersum = number1 + number2
+// console.log(number1, "+", number2, "=", number1 + number2, numbersum, "* 2 =", numbersum * 2)
+// number1 = number1 + 1
+// number2 = number2 + 2
+// numbersum = number1 + number2
+// console.log(number1, "+", number2, "=", number1 + number2, numbersum, "* 2 =", numbersum * 2)
+
+
+// function
+
+
+function sumamt(number1, number2) {
+  // console.log(number1, "+", number2, "=", number1 + number2, ",", number1 + number2, "* 2 =", number1 + number2 * 2)
+  console.log(`${number1} + ${number2} = ${number1 + number2}, ${number1 + number2} * 2 =${(number1 + number2) * 2}`)
+}
+
+sumamt(1, 2)
+sumamt(2, 4)
+sumamt(4, 5)
+sumamt(5, 10)
